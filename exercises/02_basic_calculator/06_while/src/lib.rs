35b26162f89ff587c8e0d5f6dd06d2768135c35a
ZZQ <18736271057@163.com>
2026-01-03 19:49:32 +0800

    02_07

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,10 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    while n > 1 {
+        return  n * factorial(n - 1)
+    }
+    1
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 || n == 1 {
+        return 1;
+    };
+    let mut result = 1;
+    for i in (1..=n).rev() {
+        result *= i;
+    }
+    result
 }
 
 #[cfg(test)]
